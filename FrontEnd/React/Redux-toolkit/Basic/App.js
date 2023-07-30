f928432c01619791151c8cca2b1bcb93d309d60f
성호  권 <maystar0503@naver.com>
2023-07-30 22:43:11 +0900

    Update App.js

--- FrontEnd/React/Redux-toolkit/Basic/App.js
+++ FrontEnd/React/Redux-toolkit/Basic/App.js
@@ -6,11 +6,14 @@ import {up} from './counterSlice';
 
 function Counter(){
   const dispatch = useDispatch();
+// 인자로 전달된 state는 store에서 전체
   const count = useSelector(state=>{
     return state.counter.value;
   });
   return <div>
     <button onClick={()=>{
+// dispatch ({type : 'counter/up' ,step: 2}); 와 밑에 동일
+// dispatch (counterSlice.actions.up(2)); 
       dispatch(up(2));
     }}>+</button> {count}
   </div>
