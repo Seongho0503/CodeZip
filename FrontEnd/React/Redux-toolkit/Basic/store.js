406fd6396776c93fe1403d2b978e22f99c50a9b2
성호  권 <maystar0503@naver.com>
2023-07-30 22:34:13 +0900

    Update store.js

--- FrontEnd/React/Redux-toolkit/Basic/store.js
+++ FrontEnd/React/Redux-toolkit/Basic/store.js
@@ -1,7 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit';
 import counterSlice from './counterSlice';
+
+// 작은 store(=slcie)를 하나로 모을 때 configureStore 사용
+// configureStore 객체를 전달
+// store : 하나의 거대어 store
 const store = configureStore({
+  // reducer (복수 아님)
   reducer:{
+    // counter 네임 프로퍼티 안에 있는 모든 reducer들읠 통합 한 것이 counterSlice.reducer이다
     counter:counterSlice.reducer
   }
 });
